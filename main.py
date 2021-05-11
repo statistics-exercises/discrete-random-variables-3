85b391fddf5c59207420f1ff11053953e408cdb2
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 10:12:55 +0100

    Added answer

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 
 def bernoulli(p) : 
   # Your code for generating a Bernoulli random variable goes here
-  
+  if np.random.uniform(0,1)<p : return 1
+  return 0   
+ 
 # Your code for generating the list of Bernoulli random variables goes here
 prob=0.5
+xvals, yvals = np.zeros(100),np.zeros(100)
+for i in range(100) : 
+   xvals[i] = i+1
+   yvals[i] = bernoulli(prob)
+
+plt.plot( xvals, yvals, 'ko' )
+plt.xlabel("Index")
+plt.ylabel("random variable")
+plt.savefig("graph.png")
   
